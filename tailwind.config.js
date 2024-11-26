21da4d522b6551b8998cc7e8dc1b641f012d2bb2
amingo adam <amingoadam23@gmail.com>
2024-11-26 09:22:48 +0000

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        priamry: "#4DC490",
+        secondary: "#FC1D5B",
+      },
+    },
+  },
+  plugins: [],
+};
